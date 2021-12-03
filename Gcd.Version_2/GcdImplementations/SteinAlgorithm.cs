04ce22c80a1fc2d40106b385b58db1c1e2454d11
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2021-12-03 14:32:55 +0000

    Update SteinAlgorithm.

--- Gcd.Version_2/GcdImplementations/SteinAlgorithm.cs
+++ Gcd.Version_2/GcdImplementations/SteinAlgorithm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Gcd.Version_2.Interfaces;
 
 #pragma warning disable SA1300
@@ -13,56 +13,7 @@ namespace Gcd.Version_2.GcdImplementations
         /// <exception cref="ArgumentOutOfRangeException">Thrown when one or two numbers are int.MinValue.</exception>
         public int Calculate(int first, int second)
         {
-            if (first == int.MinValue || second == int.MinValue)
-            {
-                throw new ArgumentOutOfRangeException($"Number cannot be {int.MinValue}.");
-            }
-
-            if (first == 0 && second == 0)
-            {
-                throw new ArgumentException("Two numbers cannot be 0 at the same time.");
-            }
-
-            first = Math.Abs(first);
-            second = Math.Abs(second);
-            if (first == second)
-            {
-                return first;
-            }
-
-            if (first == 0)
-            {
-                return second;
-            }
-
-            if (second == 0)
-            {
-                return first;
-            }
-
-            if ((~first & 1) == 1)
-            {
-                if ((second & 1) == 1)
-                {
-                    return this.Calculate(first >> 1, second);
-                }
-                else
-                {
-                    return this.Calculate(first >> 1, second >> 1) << 1;
-                }
-            }
-
-            if ((~second & 1) == 1)
-            {
-                return this.Calculate(first, second >> 1);
-            }
-
-            if (first > second)
-            {
-                return this.Calculate((first - second) >> 1, second);
-            }
-
-            return this.Calculate((second - first) >> 1, first);
+            Gcd. Refactoring. Help
         }
     }
 }
